b1c0cd21ae6fd1f5ee9eb528b9410ad294f1be33
hoarseProgramming <hampus.es@gmail.com>
2024-12-06 13:41:39 +0100

    add MainWindowViewModel

---
+++ ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Bookstore_App.ViewModel
+{
+    class MainWindowViewModel : ViewModelBase
+    {
+        public InventoryViewModel? InventoryViewModel { get; }
+        public MainWindowViewModel()
+        {
+            InventoryViewModel = new InventoryViewModel(this);
+        }
+    }
+}
