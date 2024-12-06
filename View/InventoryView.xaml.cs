e336728b032406c636cbbc02f9be24581c2a8659
hoarseProgramming <hampus.es@gmail.com>
2024-12-06 13:40:52 +0100

    Create view: Invientory view. Update with ability to see stores and inventory balance of choosen store

---
+++ View/InventoryView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Bookstore_App.ViewModel;
+using System.Windows.Controls;
+
+namespace Bookstore_App.View
+{
+    /// <summary>
+    /// Interaction logic for InventoryView.xaml
+    /// </summary>
+    public partial class InventoryView : UserControl
+    {
+        public InventoryView()
+        {
+            InitializeComponent();
+        }
+
+        private void TreeView_SelectedItemChanged(object sender, System.Windows.RoutedPropertyChangedEventArgs<object> e)
+        {
+            (DataContext as InventoryViewModel)?.GetAndSetInventoryBalances(e.NewValue as Store);
+        }
+
+        private void myListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var store = (sender as ListBox)?.SelectedItem as Store;
+            (DataContext as InventoryViewModel)?.GetAndSetInventoryBalances(store);
+        }
+    }
+}
