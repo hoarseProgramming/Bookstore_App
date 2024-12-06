3031367a409d25b76253ee4649c8e358a2fb50e5
hoarseProgramming <hampus.es@gmail.com>
2024-12-06 13:44:28 +0100

    Update with InventoryView user control

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,13 +1,5 @@
-﻿using System.Text;
+﻿using Bookstore_App.ViewModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Bookstore_App
 {
@@ -16,9 +8,12 @@ namespace Bookstore_App
     /// </summary>
     public partial class MainWindow : Window
     {
+        private MainWindowViewModel mainWindowViewModel = new();
         public MainWindow()
         {
             InitializeComponent();
+
+            DataContext = mainWindowViewModel;
         }
     }
 }
