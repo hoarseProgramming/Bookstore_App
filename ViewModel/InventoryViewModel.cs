457a3b99bddf0fdeb198359939a3c5192b7c7f5e
hoarseProgramming <hampus.es@gmail.com>
2024-12-11 15:20:13 +0100

    Add properties for databinding with inventoryview and methods for adding and removing inventory balances

--- ViewModel/InventoryViewModel.cs
+++ ViewModel/InventoryViewModel.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Bookstore_App.Commands;
+using Bookstore_App.Services;
+using System.Collections;
 using System.Collections.ObjectModel;
 
 namespace Bookstore_App.ViewModel
@@ -15,9 +17,13 @@ namespace Bookstore_App.ViewModel
             set
             {
                 _isInventoryMode = value;
+                ShouldOpenAddBookCommand.RaiseCanExecuteChanged();
                 RaisePropertyChanged();
             }
         }
+
+        private bool isPossibleToAddBooks = false;
+        private int numberOfBooksInCatalog = 0;
 
         private ObservableCollection<Store> _stores = new();
 
@@ -30,7 +36,19 @@ namespace Bookstore_App.ViewModel
                 RaisePropertyChanged();
             }
         }
+        private Store _activeStore;
 
+        public Store ActiveStore
+
+        {
+            get => _activeStore;
+            set
+            {
+                _activeStore = value;
+                ShouldOpenAddBookCommand.RaiseCanExecuteChanged();
+                RaisePropertyChanged();
+            }
+        }
 
         private ObservableCollection<InventoryBalance> _inventoryBalances = new();
 
@@ -44,38 +62,164 @@ namespace Bookstore_App.ViewModel
             }
         }
 
-        public InventoryViewModel(MainWindowViewModel? mainWindowViewModel)
+        private IList _selectedInventoryBalances = new ArrayList();
+
+        public IList SelectedInventoryBalances
+
         {
-            this.mainWindowViewModel = mainWindowViewModel;
-            //TODO: Should be async and sent by mainwindow loaded method?
-            GetStores();
-        }
-
-        //TODO: Make async
-        public void GetStores()
-        {
-            using var db = new BookstoreCompanyContext();
-
-            var stores = db.Stores.ToList();
-
-            foreach (var store in stores)
+            get => _selectedInventoryBalances;
+            set
             {
-                Stores.Add(store);
+                _selectedInventoryBalances = value;
+                RaisePropertyChanged();
+                RemoveInventoryBalanceCommand.RaiseCanExecuteChanged();
             }
         }
 
-        //TODO: Make async! Make as Command!?
-        public void GetAndSetInventoryBalances(Store store)
+        private InventoryBalance _activeInventoryBalance;
+
+        public InventoryBalance ActiveInventoryBalance
+
         {
+            get => _activeInventoryBalance;
+            set
+            {
+                _activeInventoryBalance = value;
+                RaisePropertyChanged();
+            }
+        }
+
+
+        private ObservableCollection<Book> _books = new();
+
+        public ObservableCollection<Book> Books
+        {
+            get => _books;
+            set
+            {
+                _books = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private Book _selectedBook;
+
+        public Book SelectedBook
+        {
+            get => _selectedBook;
+            set
+            {
+                _selectedBook = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public DelegateCommand ShouldOpenAddBookCommand { get; }
+        public EventHandler ShouldOpenAddBookMessage;
+
+        public DelegateCommand ShouldAddBookCommand { get; }
+        public EventHandler ShouldAddBookMessage;
+
+        public DelegateCommand RemoveInventoryBalanceCommand { get; }
+        public InventoryViewModel(MainWindowViewModel? mainWindowViewModel)
+        {
+            this.mainWindowViewModel = mainWindowViewModel;
+
+            ShouldOpenAddBookCommand = new DelegateCommand(ShouldOpenAddBook, CanOpenAddBook);
+            ShouldAddBookCommand = new DelegateCommand(ShouldAddBook, CanAddBook);
+            RemoveInventoryBalanceCommand = new DelegateCommand(RemoveInventoryBalance, CanRemoveInventoryBalance);
+
+            Stores.Add(new Store() { StoreName = "Loading" });
+
+        }
+        private void RemoveInventoryBalance(object obj)
+        {
+            var inventoryBalancesToRemove = new List<InventoryBalance>();
+
+            foreach (var inventoryBalance in SelectedInventoryBalances)
+            {
+                inventoryBalancesToRemove.Add((InventoryBalance)inventoryBalance);
+            }
+
+            for (int i = 0; i < inventoryBalancesToRemove.Count; i++)
+            {
+                InventoryBalances.Remove(inventoryBalancesToRemove[i]);
+            }
+
+            ShouldOpenAddBookCommand.RaiseCanExecuteChanged();
+        }
+
+        private bool CanRemoveInventoryBalance(object? args) => SelectedInventoryBalances.Count > 0;
+
+
+        public async Task AddBookAsync(int unitsInStock)
+        {
+            InventoryBalances.Add(new InventoryBalance() { Book = SelectedBook, Isbn13 = SelectedBook.Isbn13, Store = ActiveStore, StoreId = ActiveStore.Id, UnitsInStock = unitsInStock });
+
+            //SelectedBook = Books.FirstOrDefault();
+            isPossibleToAddBooks = InventoryBalances.Count < numberOfBooksInCatalog ? true : false;
+            ShouldOpenAddBookCommand.RaiseCanExecuteChanged();
+        }
+        //TODO: Jadu, ska man ha de här?
+        private void ShouldAddBook(object obj) => ShouldAddBookMessage.Invoke(this, EventArgs.Empty);
+        private bool CanAddBook(object? arg) => true;
+
+        //Delete book = Set ispossible to add book to true and raisecanexecutechanged on should open addbookcommand
+
+
+        private void ShouldOpenAddBook(object obj) => ShouldOpenAddBookMessage.Invoke(this, EventArgs.Empty);
+
+        private bool CanOpenAddBook(object? arg) => IsInventoryMode && ActiveStore is not null && InventoryBalances.Count < numberOfBooksInCatalog ? true : false;
+
+        public async Task GetAndSetBooks()
+        {
+            var books = await DataManager.GetBooksAsync();
+
+            numberOfBooksInCatalog = books.Count;
+
+            var newBooks = new ObservableCollection<Book>();
+
+            foreach (var book in books)
+            {
+                if (!(InventoryBalances.Any(i => i.Isbn13 == book.Isbn13)))
+                {
+                    newBooks.Add(book);
+                }
+            }
+
+            Books = newBooks;
+
+            SelectedBook = Books.FirstOrDefault();
+
+            isPossibleToAddBooks = InventoryBalances.Count < numberOfBooksInCatalog ? true : false;
+            ShouldOpenAddBookCommand.RaiseCanExecuteChanged();
+        }
+
+        //TODO: Make async / Command
+        public async Task GetAndSetStoresAsync()
+        {
+            var stores = await DataManager.GetStoresAsync();
+
+            var currentStores = new ObservableCollection<Store>();
+
+            foreach (var store in stores)
+            {
+                currentStores.Add(store);
+            }
+
+            Stores = currentStores;
+        }
+
+        //TODO: Make async! Make as Command!?
+        public async Task GetAndSetInventoryBalancesAsync(Store store)
+        {
+            var defaultInventoryBalance = new InventoryBalance() { Book = new Book() { Title = "Loading" } };
+            InventoryBalances = new ObservableCollection<InventoryBalance>() { defaultInventoryBalance };
+
+
             ObservableCollection<InventoryBalance> newInventoryBalances = new();
 
-            using var db = new BookstoreCompanyContext();
-
-            var inventoryBalances = db.InventoryBalances
-                .Include(i => i.Store)
-                .Include(i => i.Book)
-                .Where(i => i.Store.Equals(store))
-                .ToList();
+            var inventoryBalances = await DataManager.GetInventoryBalancesAsync(store);
 
             foreach (var inventoryBalance in inventoryBalances)
             {
@@ -84,5 +228,11 @@ namespace Bookstore_App.ViewModel
 
             InventoryBalances = newInventoryBalances;
         }
+
+        //Make to command
+        //public async void AddBookToTinventory(Store store, Book book, int numberOfUnits)
+        //{
+        //    DataManager.AddInventoryBalance(store, book, numberOfUnits);
+        //}
     }
 }
