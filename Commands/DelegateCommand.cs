6e240d11a67ed23b77080b49b75be802836bedcc
hoarseProgramming <hampus.es@gmail.com>
2024-12-11 15:16:40 +0100

    Add DelegateCommand class

---
+++ Commands/DelegateCommand.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Input;
+
+namespace Bookstore_App.Commands
+{
+    internal class DelegateCommand : ICommand
+    {
+        private readonly Action<object> execute;
+        private readonly Func<object?, bool> canExecute;
+
+        public event EventHandler? CanExecuteChanged;
+
+        public DelegateCommand(Action<object> execute, Func<object?, bool> canExecute = null)
+        {
+            ArgumentNullException.ThrowIfNull(execute);
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public void Execute(object? parameter) => execute(parameter);
+
+        public bool CanExecute(object? parameter) => canExecute is null ? true : canExecute(parameter);
+
+        public void RaiseCanExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+
+    }
+}
