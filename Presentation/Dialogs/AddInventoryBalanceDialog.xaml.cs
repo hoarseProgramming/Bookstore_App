70effd4af5558d39331e84f39547e073cb507e0b
hoarseProgramming <hampus.es@gmail.com>
2024-12-13 15:48:57 +0100

    Move to presentation/Dialogs

--- Presentation/Dialogs/AddInventoryBalanceDialog.xaml.cs
+++ Presentation/Dialogs/AddInventoryBalanceDialog.xaml.cs
@@ -1,12 +1,9 @@
-﻿using Bookstore_App.ViewModel;
+﻿using Bookstore_App.Presentation.ViewModel;
 using System.Windows;
 using System.Windows.Controls;
 
-namespace Bookstore_App.Dialogs
+namespace Bookstore_App.Presentation.Dialogs
 {
-    /// <summary>
-    /// Interaction logic for AddInventoryBalanceDialog.xaml
-    /// </summary>
     public partial class AddInventoryBalanceDialog : Window
     {
         public int selectedNumberOfUnits;
@@ -30,12 +27,24 @@ namespace Bookstore_App.Dialogs
 
         private async void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            await (DataContext as MainWindowViewModel).InventoryViewModel.GetAndSetBooks();
+            await ((MainWindowViewModel)DataContext).InventoryViewModel.GetAndSetBooksForInventoryView();
         }
 
         private void TextBox_KeyUp(object sender, System.Windows.Input.KeyEventArgs e)
         {
-            var result = Int32.TryParse((sender as TextBox).Text, out selectedNumberOfUnits);
+            TextBox? textBox = sender as TextBox;
+            bool success = false;
+
+            if (textBox is not null)
+            {
+                success = int.TryParse(textBox.Text, out selectedNumberOfUnits);
+                success = selectedNumberOfUnits >= 0;
+            }
+
+            if (!success)
+            {
+                MessageBox.Show("Invalid input! Must be a positive number!", "Wrong input!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
     }
 }
