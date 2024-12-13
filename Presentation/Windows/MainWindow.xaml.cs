1d42d8f0e178a65a7c0c2273c13bdcb9c76346c5
hoarseProgramming <hampus.es@gmail.com>
2024-12-13 15:51:21 +0100

    Move to presentation/windows

---
+++ Presentation/Windows/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using Bookstore_App.Presentation.Dialogs;
+using Bookstore_App.Presentation.ViewModel;
+using System.Windows;
+
+namespace Bookstore_App
+{
+    public partial class MainWindow : Window
+    {
+        private MainWindowViewModel mainWindowViewModel = new();
+
+        private WindowState stateBeforeFullScreenToggled;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataContext = mainWindowViewModel;
+            mainWindowViewModel.InventoryViewModel.ShowError = (message, caption) => MessageBox.Show(message, caption, MessageBoxButton.OK, MessageBoxImage.Error);
+            mainWindowViewModel.InventoryViewModel.OpenAddInventoryBalance = OpenAddInventoryBalanceDialog;
+            mainWindowViewModel.ToggleFullScreen = ToggleFullScreen;
+            Loaded += MainWindow_Loaded;
+
+        }
+
+        private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            await mainWindowViewModel.InventoryViewModel.GetAndSetStoresAsync();
+        }
+
+        public void ToggleFullScreen()
+        {
+            if (!(this.WindowState == WindowState.Maximized))
+            {
+                stateBeforeFullScreenToggled = WindowState;
+                WindowState = WindowState.Maximized;
+            }
+            else
+            {
+                WindowState = stateBeforeFullScreenToggled;
+                ResizeMode = ResizeMode.CanResize;
+            }
+        }
+
+        private async void OpenAddInventoryBalanceDialog()
+        {
+            AddInventoryBalanceDialog addInventoryBalanceDialog = new();
+
+            var result = addInventoryBalanceDialog.ShowDialog();
+
+            if (result == true)
+            {
+                await mainWindowViewModel.InventoryViewModel.AddInventoryBalanceAsync(addInventoryBalanceDialog.selectedNumberOfUnits);
+            }
+        }
+    }
+}
