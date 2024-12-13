2a4ba92ed31f712dd3d5dd619826fc331d369fea
hoarseProgramming <hampus.es@gmail.com>
2024-12-13 15:50:41 +0100

    Add menuview with inputbindings for functionality

---
+++ Presentation/View/MenuView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Bookstore_App.Presentation.View
+{
+    /// <summary>
+    /// Interaction logic for MenuView.xaml
+    /// </summary>
+    public partial class MenuView : UserControl
+    {
+        public MenuView()
+        {
+            InitializeComponent();
+        }
+    }
+}
