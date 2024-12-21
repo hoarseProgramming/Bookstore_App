bf97839464766ac0c47435d259818a44ca0b9d8e
hoarseProgramming <hampus.es@gmail.com>
2024-12-21 21:27:05 +0100

    moved out from data manager class

---
+++ Services/CatalogInfo.cs
@@ -0,0 +1,31 @@
+﻿using Bookstore_App.Domain;
+using Bookstore_App.Presentation.ViewModel;
+using System.Collections.ObjectModel;
+
+namespace Bookstore_App.Services;
+
+
+public class CatalogInfo
+{
+    public CatalogInfo
+        (ObservableCollection<BookViewModel>? books, ObservableCollection<Author>? authors,
+        ObservableCollection<SubCategory>? subCategories, ObservableCollection<Language>? languages,
+        ObservableCollection<PrimaryAudience>? primaryAudiences, ObservableCollection<PublishingHouse>? publishingHouses)
+    {
+        Books = books;
+        Authors = authors;
+        SubCategories = subCategories;
+        Languages = languages;
+        PrimaryAudiences = primaryAudiences;
+        PublishingHouses = publishingHouses;
+    }
+
+    public ObservableCollection<BookViewModel>? Books { get; set; }
+    public ObservableCollection<Author>? Authors { get; set; }
+    public ObservableCollection<SubCategory>? SubCategories { get; set; }
+    public ObservableCollection<Language>? Languages { get; set; }
+    public ObservableCollection<PrimaryAudience>? PrimaryAudiences { get; set; }
+    public ObservableCollection<PublishingHouse>? PublishingHouses { get; set; }
+}
+
+
